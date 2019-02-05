dfbf6fdff5f4e1ab4b608084896d3fa0b9cba2bc
Kenneth Watson <git@shrimpworks.za.net>
2019-02-05 07:07:33 +0200

    tweak travis build, add verification build step

---
+++ bin/verify.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+UA_VERSION=latest
+UA_BIN=/tmp/unreal-archive
+
+set -e
+
+# download unreal-archive binary
+if [ ! -f ${UA_BIN} ]; then
+  echo "Downloading unreal-archive binary"
+  curl https://code.shrimpworks.za.net/artefacts/unreal-archive/${UA_VERSION}/unreal-archive --output ${UA_BIN}
+  chmod +x ${UA_BIN}
+fi
+
+# execute unreal-archive www command
+echo "Verify content"
+${UA_BIN} summary --content-path=$1 --store=NOP
